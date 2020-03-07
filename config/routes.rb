cfef18a1a27f135d08f955ec28cfdd18bd5c6dd5
keiji77 <57708959+keiji77@users.noreply.github.com>
2020-03-07 14:29:02 +0900

    ルーティングの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-
+  #一覧画面へのルーティング
+  get '/users', to: 'users#index'
+  #新規登録画面へのルーティング
+  get '/users/new', to:'users#new'
+  #新規登録を行うためのルーティング,登録機能をつけるためのルーティング,post:データを投稿するときのHTTPメソッド
+  post '/users', to:'users#create'
+  #編集画面へのルーティング
+  get '/users/:id/edit', to:'users#edit'
+  #更新を行うためのルーティング
+  patch '/users/:id', to:'users#update'
+  #削除を行うためのルーティング
+  delete '/users/:id', to: 'users#destroy'
 end
